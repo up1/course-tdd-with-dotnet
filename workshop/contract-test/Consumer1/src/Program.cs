898a98169fa3b0482da8c242d7232ed9098fd3ae
up1 <somkiat.p@gmail.com>
2022-10-20 00:04:19 +0700

    Demo with consumer 1

---
+++ workshop/contract-test/Consumer1/src/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using src;
+
+string baseUrl = "https://localhost:7072";
+
+var gateway = new ServiceAGateway(baseUrl);
+
+try
+{
+
+    var result = await gateway.CallApi(1);
+    var resultContentText = result.Content!.ReadAsStringAsync().GetAwaiter().GetResult();
+    Console.WriteLine(resultContentText);
+}catch(Exception e)
+{
+    Console.WriteLine("Error, cannot call api with " + e.Message);
+}
---
+++ workshop/contract-test/Consumer1/src/ServiceAGateway.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace src
+{
+    public class ServiceAGateway
+    {
+        private string baseUrl;
+
+        public ServiceAGateway(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public async Task<HttpResponseMessage> CallApi(int userId)
+        {
+            using (var client = new HttpClient { BaseAddress = new Uri(baseUrl) })
+            {
+                try
+                {
+                    var response = await client.GetAsync($"/api/user/{userId}");
+                    return response;
+                }
+                catch (System.Exception e)
+                {
+                    throw new Exception("There was a problem connecting to service A.", e);
+                }
+            }
+        }
+    }
+}
+
