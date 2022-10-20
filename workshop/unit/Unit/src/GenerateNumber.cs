0f2a8ddfa8ff5f044b938f380b520f31ec047cf8
up1 <somkiat.p@gmail.com>
2022-10-20 15:25:13 +0700

    Start workshop of unit test

---
+++ workshop/unit/Unit/src/GenerateNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace src
+{
+    public class GenerateNumber
+    {
+        public string getResult()
+        {
+            Random random = new Random();
+            int number = random.Next(10);
+            return "No" + number;
+        }
+
+    }
+}
+
---
+++ workshop/unit/Unit/tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using src;
+
+namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestWithRandom5()
+    {
+        // Arrange
+        GenerateNumber g = new GenerateNumber();
+        // Act
+        string actualResult = g.getResult();
+        // Assert
+        Assert.Equal("No5", actualResult);
+    }
+
+
+}
